c1b3c6dc67b1ef07ff8c0e1d7129d39680410c06
nerrma <sina.alsharif@gmail.com>
2024-05-28 16:43:39 +1000

    unrolling, not really faster

--- basic_hashmap_linear.hpp
+++ basic_hashmap_linear.hpp
@@ -22,15 +22,69 @@ inline auto simd_compare(const std::string_view &a,
   const auto sz = a.size();
   alignas(8) const auto a_dat = a.data();
   alignas(8) const auto b_dat = b.data();
-  for (int i = 0; i < sz; i += 32) {
-    __m256i a_chunk = _mm256_loadu_si256((__m256i *)&a_dat[i]);
-    __m256i b_chunk = _mm256_loadu_si256((__m256i *)&b_dat[i]);
-    uint8_t shift_by = 32 * (i + 1) > sz ? (sz - i) : 32;
 
+  if (sz < 32) {
+    __m256i a_chunk = _mm256_loadu_si256((__m256i *)&a_dat[0]);
+    __m256i b_chunk = _mm256_loadu_si256((__m256i *)&b_dat[0]);
+
+    uint8_t shift_by = sz;
     __m256i m = _mm256_cmpeq_epi8(a_chunk, b_chunk) << shift_by;
-    if (_mm256_testz_si256(a_chunk, b_chunk)) {
+    return !_mm256_testz_si256(m, m);
+  } else if (sz > 32 && sz < 32 * 2) {
+    __m256i a_chunk1 = _mm256_loadu_si256((__m256i *)&a_dat[0]);
+    __m256i b_chunk1 = _mm256_loadu_si256((__m256i *)&b_dat[0]);
+    __m256i a_chunk2 = _mm256_loadu_si256((__m256i *)&a_dat[32]);
+    __m256i b_chunk2 = _mm256_loadu_si256((__m256i *)&b_dat[32]);
+
+    __m256i m = _mm256_cmpeq_epi8(a_chunk1, b_chunk1);
+    /* check if first chunk equal */
+    if (_mm256_testz_si256(m, m)) {
       return false;
     }
+
+    uint8_t shift_by = sz - 32;
+    __m256i m2 = _mm256_cmpeq_epi8(a_chunk2, b_chunk2) << shift_by;
+    return !_mm256_testz_si256(m2, m2);
+  } else if (sz > 32 * 2 && sz < 32 * 3) {
+    __m256i a_chunk1 = _mm256_loadu_si256((__m256i *)&a_dat[0]);
+    __m256i b_chunk1 = _mm256_loadu_si256((__m256i *)&b_dat[0]);
+    __m256i a_chunk2 = _mm256_loadu_si256((__m256i *)&a_dat[32]);
+    __m256i b_chunk2 = _mm256_loadu_si256((__m256i *)&b_dat[32]);
+    __m256i a_chunk3 = _mm256_loadu_si256((__m256i *)&a_dat[64]);
+    __m256i b_chunk3 = _mm256_loadu_si256((__m256i *)&b_dat[64]);
+
+    __m256i m1 = _mm256_cmpeq_epi8(a_chunk1, b_chunk1);
+    __m256i m2 = _mm256_cmpeq_epi8(a_chunk2, b_chunk2);
+    /* check if first chunk equal */
+    if (_mm256_testz_si256(m1, m1) || _mm256_testz_si256(m2, m2)) {
+      return false;
+    }
+
+    uint8_t shift_by = sz - 64;
+    __m256i m3 = _mm256_cmpeq_epi8(a_chunk3, b_chunk3) >> shift_by;
+    return !_mm256_testz_si256(m3, m3);
+  } else if (sz > 32 * 3 && sz < 32 * 4) {
+    __m256i a_chunk1 = _mm256_loadu_si256((__m256i *)&a_dat[0]);
+    __m256i b_chunk1 = _mm256_loadu_si256((__m256i *)&b_dat[0]);
+    __m256i a_chunk2 = _mm256_loadu_si256((__m256i *)&a_dat[32]);
+    __m256i b_chunk2 = _mm256_loadu_si256((__m256i *)&b_dat[32]);
+    __m256i a_chunk3 = _mm256_loadu_si256((__m256i *)&a_dat[64]);
+    __m256i b_chunk3 = _mm256_loadu_si256((__m256i *)&b_dat[64]);
+    __m256i a_chunk4 = _mm256_loadu_si256((__m256i *)&a_dat[96]);
+    __m256i b_chunk4 = _mm256_loadu_si256((__m256i *)&b_dat[96]);
+
+    __m256i m1 = _mm256_cmpeq_epi8(a_chunk1, b_chunk1);
+    __m256i m2 = _mm256_cmpeq_epi8(a_chunk2, b_chunk2);
+    __m256i m3 = _mm256_cmpeq_epi8(a_chunk3, b_chunk3);
+    /* check if first chunk equal */
+    if (_mm256_testz_si256(m1, m1) || _mm256_testz_si256(m2, m2) ||
+        _mm256_testz_si256(m3, m3)) {
+      return false;
+    }
+
+    uint8_t shift_by = sz - 96;
+    __m256i m4 = _mm256_cmpeq_epi8(a_chunk4, b_chunk4) >> shift_by;
+    return !_mm256_testz_si256(m4, m4);
   }
 
   return true;
